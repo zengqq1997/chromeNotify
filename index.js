378a08ead50103cd8d1ef4cd29cb1ddefb7f30ca
zengqq@yeastar.com <zengqq@yeastar.com>
2023-03-10 10:56:09 +0800

    feat: 新增一个大小版本更新区别

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const axios = require("axios");
 const execSync = require("./exec");
-const { WEIXIN_WEBHOOK, WEIXIN_WEBHOOK1, MOBILE } = require("./utils/env");
+const {
+    WEIXIN_WEBHOOK,
+    WEIXIN_WEBHOOK1,
+    WEIXIN_WEBHOOK2,
+    MOBILE,
+    MOBILE2,
+} = require("./utils/env");
 const { sendMail } = require("./utils/mail");
 
 // 创建忽略 SSL 的 axios 实例
@@ -22,27 +28,50 @@ instance({ url: "/channels" }, (error, response, data) => {
             chromeData = result.data;
         }
         const stable = chromeData?.stable ?? {};
-        const stableUpdateTime = stable?.next_stable_refresh
+        const beta = chromeData?.beta ?? {};
+        const stableUpdateTime = stable?.stable_date
             ? stable?.next_stable_refresh
             : stable?.stable_date;
+        const betaUpdateTime = beta?.stable_date;
         const version = stable?.mstone;
+        const betaVersion = beta?.mstone;
         const time2date = new Date(stableUpdateTime);
+        const betaTime2date = new Date(betaUpdateTime);
         // 因为谷歌上的升级时间表上的时间与预期会延迟个一天，大概是时区和地区更新不一致，所以将获取到的日期加一天
         time2date.setDate(time2date.getDate() + 1);
+        betaTime2date.setDate(betaTime2date.getDate() + 1);
+
         const year = time2date.getFullYear();
         const month = time2date.getMonth();
         const date = time2date.getDate();
         const nowDate = new Date();
 
-        if (month === nowDate.getMonth() && date === nowDate.getDate()) {
-            sendHookMessage(
-                `今日谷歌浏览器有新版本，请注意更新, ${version}`,
-                MOBILE ? [`${MOBILE}`] : ""
-            );
+        const betaYear = time2date.getFullYear();
+        const betaMonth = time2date.getMonth();
+        const betaDate = time2date.getDate();
+
+        if (
+            month === nowDate.getMonth() &&
+            date === nowDate.getDate() &&
+            year === nowDate.getFullYear()
+        ) {
+            if (year === betaYear && betaMonth === month && date === betaDate) {
+                sendHookMessage(
+                    `请注意，今日谷歌浏览器有大版本更新，更新版本, ${betaVersion}`,
+                    MOBILE ? [`${MOBILE}`, `${MOBILE2}`] : ""
+                );
+            } else
+                sendHookMessage(
+                    `请注意今日谷歌浏览器有小版本，更新版本， ${version}`,
+                    MOBILE ? [`${MOBILE}`, `${MOBILE2}`] : ""
+                );
         } else {
             sendHookMessage(
                 `谷歌浏览器下次更新时间:${year}-${month}-${date}`,
-                MOBILE ? [`${MOBILE}`] : ""
+                MOBILE ? [`${MOBILE}`, `${MOBILE2}`] : "",
+                "text",
+                // 每日
+                WEIXIN_WEBHOOK2
             );
         }
     })
@@ -51,6 +80,7 @@ instance({ url: "/channels" }, (error, response, data) => {
             `警告：接口请求异常，请及时处理\n ${err}`,
             MOBILE ? [`${MOBILE}`] : "",
             "text",
+            //康复
             WEIXIN_WEBHOOK1
         );
     });
@@ -68,10 +98,15 @@ const sendHookMessage = (
             mentioned_mobile_list: mentionedMobileList,
         },
     });
-    const cmd = `curl '${WEIXIN_WEBHOOK}' -H 'Content-Type: application/json' -d '${objStr}'`;
-    const cmd1 = `curl '${hookUrl}' -H 'Content-Type: application/json' -d '${objStr}'`;
-    const { error, stdout } = execSync(cmd);
-    if (error) sendMail();
-    const { error1, stdout1 } = execSync(cmd1);
-    if (error1) sendMail();
+    if (!hookUrl) {
+        // chrome提醒
+        const cmd = `curl '${WEIXIN_WEBHOOK2}' -H 'Content-Type: application/json' -d '${objStr}'`;
+        const { error, stdout } = execSync(cmd);
+        if (error) sendMail();
+    }
+    if (hookUrl) {
+        const cmd1 = `curl '${hookUrl}' -H 'Content-Type: application/json' -d '${objStr}'`;
+        const { error1, stdout1 } = execSync(cmd1);
+        if (error1) sendMail();
+    }
 };
--- utils/env.js
+++ utils/env.js
@@ -5,9 +5,14 @@ module.exports = {
    * 企业微信机器人配置
    * https://developer.work.weixin.qq.com/document/path/91770
    */
+  // 日常通知
   WEIXIN_WEBHOOK: env.WEIXIN_WEBHOOK,
+  // 异常通知
   WEIXIN_WEBHOOK1: env.WEIXIN_WEBHOOK1,
+  // 版本更新通知
+  WEIXIN_WEBHOOK2: env.WEIXIN_WEBHOOK2,
   MAIL: env.MAIL,
   MAIL_PWD: env.MAIL_PWD,
-  MOBILE: env.MOBILE
+  MOBILE: env.MOBILE,
+  MOBILE2: env.MOBILE2,
 };
