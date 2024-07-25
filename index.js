4b17fd956f2c4476a5cfc7e62c9bd5d74fc2f256
zengqq@yeastar.com <zengqq@yeastar.com>
2024-07-25 11:37:05 +0800

    feat: 更新

--- index.js
+++ index.js
@@ -21,42 +21,14 @@ const instance = axios.create({
     },
 });
 
-// 确保缓存目录存在
-const cacheDir = 'cache';
-if (!fs.existsSync(cacheDir)) {
-    fs.mkdirSync(cacheDir);
-}
-
-// 定义存储版本数据的文件路径
-const versionDataFile = `${cacheDir}/version_data.json`;
-
-// 读取本地版本数据
-const readLocalVersionData = () => {
-    if (fs.existsSync(versionDataFile)) {
-        const data = fs.readFileSync(versionDataFile);
-        console.log(5555, data, JSON.parse(data))
-        return JSON.parse(data);
-    }
-    return null;
-};
-
-// 保存版本数据到本地文件
-const saveLocalVersionData = (data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(versionDataFile, JSON.stringify(data, null, 2), (err) => {
-            if (err){
-                console.error(err);
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    })
-};
 
 // 发送钩子消息
-const sendHookMessage = (content, mentionedMobileList = ["@all"], msgtype = "text", hookUrl) => {
-    return;
+const sendHookMessage = (
+    content,
+    mentionedMobileList = ["@all"],
+    msgtype = "text",
+    hookUrl
+) => {
     const objStr = JSON.stringify({
         msgtype,
         text: {
@@ -74,39 +46,54 @@ const sendHookMessage = (content, mentionedMobileList = ["@all"], msgtype = "tex
 // 获取 Chrome 版本信息
 const checkChromeVersion = async () => {
     try {
-        const response = await instance.get("https://versionhistory.googleapis.com/v1/chrome/platforms/win64/channels/stable/versions/all/releases?filter=endtime=none");
+        const response = await instance.get(
+            "https://versionhistory.googleapis.com/v1/chrome/platforms/win64/channels/stable/versions/all/releases?filter=endtime=none"
+        );
         const resultData = response.data ?? {};
         const { releases = [] } = resultData;
         const nowVersionObj = releases.find((item) => item.pinnable);
-        
+
         if (!nowVersionObj) {
             console.error("未找到可用的版本信息");
             return;
         }
 
-        const { version } = nowVersionObj;
-        const mainVersion = version.split(".")[0];
-        
-        // 读取本地版本数据
-        const localData = readLocalVersionData();
-        console.log(11111, localData)
-        const localVersion = localData ? localData.split(".")[0] : null;
+        const {
+            version,
+            serving: { startTime },
+        } = nowVersionObj;
 
+        const nowDate = new Date()
+        console.log("🚀 ~ checkChromeVersion ~ nowDate:", nowDate);
+        const startTimeDate = new Date(startTime)
+        console.log("🚀 ~ checkChromeVersion ~ startTimeDate:", startTimeDate);
+        const diffTime = nowDate.getTime() - startTimeDate.getTime();
+        // 向上取整
+        const diffDays = Math.ceil(diffTime / (24 * 3600 * 1000));
+        console.log(`距离上次更新 ${diffDays} 天`);
         // 判断是否有更新
-        if (!localVersion || mainVersion > localVersion) {
-            // 更新本地版本
-            console.log(2222, version)
-            await saveLocalVersionData(version);
-            const localData1 = readLocalVersionData();
-            console.log(3333, localData1)
-            sendHookMessage(`请注意今日谷歌浏览器有版本更新，版本号：${version}`, [process.env.MOBILE, process.env.MOBILE2]);
+        if (diffDays <= 1) {
+            sendHookMessage(
+                `请注意今日谷歌浏览器有版本更新，版本号：${version}`,
+                [process.env.MOBILE, process.env.MOBILE2]
+            );
         } else {
             // 康复
-            sendHookMessage("谷歌浏览器今日无更新", [process.env.MOBILE, process.env.MOBILE2], "text", process.env.WEIXIN_WEBHOOK1);
+            sendHookMessage(
+                `距离上次更新 ${diffTime} 天`,
+                [process.env.MOBILE, process.env.MOBILE2],
+                "text",
+                process.env.WEIXIN_WEBHOOK1
+            );
         }
     } catch (err) {
         // // 每日
-        sendHookMessage(`警告：接口请求异常，请及时处理\n ${err.message}`, [process.env.MOBILE], "text", process.env.WEIXIN_WEBHOOK);
+        sendHookMessage(
+            `警告：接口请求异常，请及时处理\n ${err.message}`,
+            [process.env.MOBILE],
+            "text",
+            process.env.WEIXIN_WEBHOOK
+        );
     }
 };
 
@@ -166,7 +153,7 @@ const checkIn = () => {
         });
 };
 
-// checkIn();
+checkIn();
 
 // 执行版本检查
 checkChromeVersion();
